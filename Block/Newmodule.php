cec01fbfbbb6b8ba8d8a1777c314b3a0a2d1dbec
xuanhai98 <xuanhai6622@gmail.com>
2019-09-29 12:43:51 +0700

    add thuc hanh 4 5

---
+++ Block/Newmodule.php
@@ -0,0 +1,9 @@
+<?php
+namespace Magenest\FrontendTest\Block;
+class Newmodule extends \Magento\Framework\View\Element\Template
+{
+    public function _prepareLayout()
+    {
+        return parent::_prepareLayout();
+    }
+}
